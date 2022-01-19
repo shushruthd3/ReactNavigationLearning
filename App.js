26819c3aa591adccb974ec5341e7e75d96b69a1f
shushruthd3 <97980914+shushruthd3@users.noreply.github.com>
2022-01-19 12:42:56 +0530

    Add files via upload
    First Commit to Repo

---
+++ App.js
@@ -0,0 +1,52 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+//const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+function DetailScreen({ navigation }) {
+
+  return (
+    <View>
+      <Text> Detail screen! </Text>
+
+    </View>
+  );
+}
+
+
+function HomeScreen({ navigation }) {
+
+  return (
+    <View>
+      <Text> Home screen! </Text>
+
+    </View>
+  );
+}
+export default function App() {
+
+
+  return (
+
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeScreen} options={{tabBarIcons}}/>
+        <Tab.Screen name="Detail" component={DetailScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
